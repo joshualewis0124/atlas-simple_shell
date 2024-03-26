5405646b76d6a058a81f28620ef0f46335b4c822
joshualewis0124 <7211@holbertonstudents.com>
2024-03-26 15:21:34 +0000

    main loop

--- shell.c
+++ shell.c
@@ -3,23 +3,6 @@
 #include "main.h"
 
 /**
- * spacesCheck - check if str contain only space
- * @str: string to check
- * Return: 0 on success or 1 on failure
- */
-
-int spacesCheck(const char *str)
-{
-	while (*str)
-	{
-		if (*str != ' ')
-			return (0);
-		str++;
-	}
-	return (1);
-}
-
-/**
  * main - main function for the shell
  * Return: 0 on success
  */
@@ -30,4 +13,27 @@ int main(void)
 	char *args[64] = { NULL };
 	size_t inputSize = 0;
 	ssize_t inputRead;
+	
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			printf("$jlshell ");
+			fflush(stdout);
+		}
 
+		inputRead = getline(&input, &inputSize, stdin);
+		if (inputRead == EOF)
+		{
+			free(input);
+			exit(0);
+		}
+
+		if (inputRead > 0 && input[inputRead - 1] == '\n')
+			input[inputRead - 1] = '\0';
+		if (spacesCheck(input) != 1)
+			tokenize(input, args);
+	}
+	free(input);
+	return (0);
+}
