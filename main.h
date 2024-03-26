1cf4780db84c84fc69b202acafb765e29dd8d36a
joshualewis0124 <7211@holbertonstudents.com>
2024-03-26 14:52:59 +0000

    header file creation

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sched.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+#endif
