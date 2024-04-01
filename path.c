36b75b8c4e3273d67fd205e7836cd8aeb915d6aa
joshualewis0124 <7211@holbertonstudents.com>
2024-04-01 23:03:25 +0000

    declare function

---
+++ path.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * handle_path - Finds and handles the path of the command to execute
+ * @input: User input
+ * Return: The full path of the command if found, otherwise NULL
+ */
+
+char *handle_path(char *input)
