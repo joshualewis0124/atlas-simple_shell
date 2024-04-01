1663dd0c9c7f6854902ae5d8244afc8a1c47c3e4
joshualewis0124 <7211@holbertonstudents.com>
2024-04-01 22:33:18 +0000

    create prototype

---
+++ tokenize.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * tokenize - function that splits a string into tokens
+ * @input: user input
+ * @args: arguments
+ * Return: void
+ */
+
+void tokenize(char *input, char *args[])
+{
+
